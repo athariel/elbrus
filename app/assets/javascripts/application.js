9e2ff2c1e28feed738fcd5ede185eea0f8b9002d
Ola Kalinowska <athariel@gmail.com>
2016-01-31 14:40:57 +0100

    theme integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require flatty/jquery/jquery.min
+//= require flatty/jquery/jquery.mobile.custom.min
+//= require flatty/jquery/jquery-migrate.min
+//= require flatty/jquery/jquery-ui.min
+//= require flatty/bootstrap/bootstrap.min
+//= require flatty/plugins/plugins
+//= require flatty/theme
 //= require turbolinks
-//= require_tree .
